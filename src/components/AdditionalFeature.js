c6f67a9f1d51a2f4f08e4a72df8bf1493b94f4a2
ph5500 <philfives@gmail.com>
2020-04-14 14:54:28 -0600

    completed step 1 by moving the given state to the reducer, set up redux flow i think, and user is able to add features to their car

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { addFeature } from '../actions/featActions';
+
+
+
 
 const AdditionalFeature = props => {
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
+      {/* add onClick button and pass in props.addFeature(props.feature)} */}
       <button className="button">Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
